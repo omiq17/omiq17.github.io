fcda8aeac2555b3c0fa3104013fd3375cddb77de
omiq17 <omiq17@gmail.com>
2017-07-27 21:39:38 +0600

    portfolio, pic, js... fixing

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-       $("html").niceScroll({
+$(document).ready(function(){
+    $("html").niceScroll({
         scrollspeed: 51,
         mousescrollstep: 45,
-       });
+    });
 
-      $(function() {
-          $('a.page-scroll').bind('click', function(event) {
-              var $anchor = $(this);
-              $('html, body').stop().animate({
-                  scrollTop: $($anchor.attr('href')).offset().top
-              }, 1000);
-              event.preventDefault();
-          });
-      });
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+    var headerHeight = $(".s").outerHeight(true);
+    $(" #about, #home, #portfolio").css("padding-top", headerHeight )
+});
+       
