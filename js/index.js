05cd127dd33a5c7fcd2739a3c3b965323713f53b
omiq17 <omiq17@gmail.com>
2020-02-06 01:24:46 +0600

    3d box added and generated from array.

--- js/index.js
+++ js/index.js
@@ -22,9 +22,21 @@ $(document).ready(function() {
 	var headerHeight = $(".s").outerHeight(true);
 	$(" #about, #home, #portfolio").css("padding-top", headerHeight);
 	$(" #about, #portfolio").css("padding-bottom", headerHeight);
+
+	$(".box__face")
+		.on("click", function(event) {
+			// console.log($(this).parent()[0]);
+			rotate($(this).parent()[0]);
+		})
+		// Do not rotate on click to link
+		.on("click", "a", function(event) {
+			event.stopPropagation();
+		});
 });
 
+// Rotate the box at 180*
 function rotate(element) {
+	console.log(element);
 	let counter = element.classList[1];
 	let angleValue = 180 * counter;
 	let newCounter = parseInt(counter) + 1;
@@ -33,5 +45,5 @@ function rotate(element) {
 		.removeClass(counter)
 		.addClass(newCounter.toString());
 
-	$(element).css("transform", "translateZ(-100px) rotateY(" + angleValue + "deg)");
+	$(element).css("transform", "translateZ(-100px) rotateY(-" + angleValue + "deg)");
 }
---
+++ js/projects.js
@@ -0,0 +1,67 @@
+let projects = [
+	{
+		title: "Joyoddhoney",
+		caption: "A cultural website.",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/q_auto/v1501085935/portfolio/Screenshot_from_2017-07-26_22-45-10.jpg",
+		link: "http://joyoddhoney.herokuapp.com"
+	},
+	{
+		title: "Random Quote Machine",
+		caption: "A quotation generator.",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/q_auto/v1501085935/portfolio/Selection_004.jpg",
+		link: "https://s.codepen.io/omiq17/full/GZpGRN"
+	},
+	{
+		title: "Tic Tac Toe",
+		caption: "A game.",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/q_auto/v1501085935/portfolio/screenshot.jpg",
+		link: "http://raqib.me/tictactoe"
+	},
+	{
+		title: "Tribute To Tareque Masud",
+		caption: "A tribute page for Tareque Masud.",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/q_auto/v1501085935/portfolio/Selection_006.jpg",
+		link: "https://s.codepen.io/omiq17/full/JGVyQz"
+	}
+];
+
+projects.forEach(function(project) {
+	let scene =
+		'<div class="scene">' +
+		'<div class="box 1">' +
+		// '<div class="box 1"    onclick="rotate(this)">' +
+		'<div class="box__face box__face--front">' +
+		"<img " +
+		'alt="' +
+		project.title.toLowerCase() +
+		'"' +
+		'src="' +
+		project.image +
+		'"' +
+		'class="img-responsive"' +
+		"/>" +
+		"</div>" +
+		'<div class="box__face box__face--back">' +
+		"<h2>" +
+		project.title +
+		"</h2>" +
+		"<p>" +
+		project.caption +
+		"</p>" +
+		'<a href="' +
+		project.link +
+		'" target="_blank">checkout</a>' +
+		"</div>" +
+		'<div class="box__face box__face--right"></div>' +
+		'<div class="box__face box__face--left"></div>' +
+		'<div class="box__face box__face--top"></div>' +
+		'<div class="box__face box__face--bottom"></div>' +
+		"</div>" +
+		"</div>";
+
+	$(".showcase").append(scene);
+});
