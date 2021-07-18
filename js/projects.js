cb471c864be663d9009ebb986780d01ea6336b47
omiq17 <omiq17@gmail.com>
2021-07-18 14:58:12 +0600

    Add and update porftfolio projects.

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,32 @@
 let projects = [
+	// {
+	// 	title: "Wisekeep",
+	// 	caption: "Storage mangament system",
+	// 	type: "Professional project, role: Frontend Developer",
+	// 	image:
+	// 		"https://res.cloudinary.com/omiq17/image/upload/c_fit,h_500,w_800/v1581828850/portfolio/wisekeep.png",
+	// 	link: "https://wisekeep.hk/"
+	// },
 	{
-		title: "Wisekeep",
-		caption: "Storage mangament system",
-		type: "Professional project, role: Frontend Developer",
+		title: "Engaze.ai",
+		caption: "Restaurant chatbot & order management",
+		type: "Professional project, role: Frontend Developer (React)",
 		image:
-			"https://res.cloudinary.com/omiq17/image/upload/c_fit,h_500,w_800/v1581828850/portfolio/wisekeep.png",
-		link: "https://wisekeep.hk/"
+			"https://res.cloudinary.com/omiq17/image/upload/v1626597875/portfolio/portfolio1.png",
+		link: "https://engaze.ai/"
+	},
+	{
+		title: "Grameen Shakti",
+		caption: "Official website Grameen Shakti",
+		type: "Professional project, role: Full Stack Developer (React, Node)",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/v1626597960/portfolio/portfolio2.png",
+		link: "https://www.gshakti.org/"
 	},
 	{
 		title: "Doctors Manga",
 		caption: "Android Mobile App for reading Doctors related comics",
-		type: "Professional project, role: React Native App Developer",
+		type: "Professional project, role: App Developer (React Native)",
 		image:
 			"https://res.cloudinary.com/omiq17/image/upload/v1581836391/portfolio/doctors_manga.png",
 		link: "https://play.google.com/store/apps/details?id=co.jp.doctors_manga"
@@ -23,14 +39,6 @@ let projects = [
 			"https://res.cloudinary.com/omiq17/image/upload/v1501087942/portfolio/joyoddhoney.png",
 		link: "http://joyoddhoney.herokuapp.com"
 	},
-	// {
-	// 	title: "Random Quote Machine",
-	// 	caption: "Quotation generator",
-	// 	type: "Personal project",
-	// 	image:
-	// 		"https://res.cloudinary.com/omiq17/image/upload/v1501088115/portfolio/quote-machine.png",
-	// 	link: "https://s.codepen.io/omiq17/full/GZpGRN"
-	// },
 	{
 		title: "Tic Tac Toe",
 		caption: "Paper-and-pencil game",
@@ -52,10 +60,18 @@ let projects = [
 		type: "Personal project",
 		image: "https://res.cloudinary.com/omiq17/image/upload/v1501088129/portfolio/tribute.png",
 		link: "https://s.codepen.io/omiq17/full/JGVyQz"
-	}
+	},
+	{
+		title: "Random Quote Machine",
+		caption: "Quotation generator",
+		type: "Personal project",
+		image:
+			"https://res.cloudinary.com/omiq17/image/upload/v1501088115/portfolio/quote-machine.png",
+		link: "https://s.codepen.io/omiq17/full/GZpGRN"
+	},
 ];
 
-projects.forEach(function(project) {
+projects.forEach(function (project) {
 	let scene =
 		'<div class="scene">' +
 		'<div class="box 1">' +
@@ -67,7 +83,7 @@ projects.forEach(function(project) {
 		'src="' +
 		project.image +
 		'"' +
-		'class="img-responsive"' +
+		'class="img-responsive image"' +
 		"/>" +
 		"</div>" +
 		'<div class="box__face box__face--back">' +
